aba662ca0a409f19e50bb0fbac9b310524ee3612
ouss-issib <oussamaissib@gmail.com>
2025-11-24 20:54:43 +0100

    core : initialise the project by binding secret key of my open ai

---
+++ src/main/java/ma/ouss/chatbotaimcp/ChatbotAiMcpApplication.java
@@ -0,0 +1,13 @@
+package ma.ouss.chatbotaimcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAiMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotAiMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ouss/chatbotaimcp/ChatbotAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ouss.chatbotaimcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotAiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
