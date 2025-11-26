b5bdba6bb9b3ff7196e3886ae77e17336d0cd637
ouss-issib <oussamaissib@gmail.com>
2025-11-26 21:09:16 +0100

    feat : binding mcp client with the server done

--- src/main/java/ma/ouss/chatbotaimcp/agents/AiAgent.java
+++ src/main/java/ma/ouss/chatbotaimcp/agents/AiAgent.java
@@ -1,18 +1,22 @@
 package ma.ouss.chatbotaimcp.agents;
 
-import ma.ouss.chatbotaimcp.tools.AITools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
 
 @Component
 public class AiAgent {
     private ChatClient chatClient;
 
-    public AiAgent(ChatClient.Builder builder, ChatMemory memory, AITools tools) {
+    public AiAgent(ChatClient.Builder builder, ChatMemory memory, ToolCallbackProvider tools) {
+        Arrays.stream(tools.getToolCallbacks()).forEach(t-> System.out.println(t.getToolDefinition()));
         this.chatClient = builder
                 .defaultSystem("""
                         Vous êtes un agent qui se charge de répondre aux questions des utilisateurs 
@@ -21,7 +25,7 @@ public class AiAgent {
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(memory).build()
                 )
-                .defaultTools(tools)
+                .defaultToolCallbacks(tools)
                 .build();
     }
 
